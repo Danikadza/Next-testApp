161ab4efbb9800bb8074701946d6338760d5e01e
Danikadza <gurianov2000@yandex.ru>
2024-10-28 22:37:59 +0300

    feat/ добавил картинки для лайка и дислайка

--- src/app/components/Rating.tsx
+++ src/app/components/Rating.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Image from "next/image";
 
 const Rating = () => {
     return (
         <div className="rating">
-            <button  className="rating__likes">Like</button>
-            <button  className="rating__dislikes">Trash</button>
+            <button  className="rating__likes"><Image src="/Like.svg" alt="like" width={13} height={11}/>Like</button>
+            <button  className="rating__dislikes"><Image src="/Dislike.svg" alt="like" width={13} height={11}/>Trash</button>
         </div>
     )
 }
