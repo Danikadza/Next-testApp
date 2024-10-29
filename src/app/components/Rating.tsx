ea02ef1dc849d364fb1c899a2bda13dfbb615072
Danikadza <gurianov2000@yandex.ru>
2024-10-29 18:12:39 +0300

    feat/ изменение цвета кнопок лайка и дизлайка

--- src/app/components/Rating.tsx
+++ src/app/components/Rating.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 
 interface RatingProps {
@@ -6,11 +8,30 @@ interface RatingProps {
     dislikes: number;
 }
 
-const Rating = ({likes, dislikes} : RatingProps) => {
+const Rating = ({ likes, dislikes }: RatingProps) => {
+
+    const [rating, setRating] = useState<string>();
+
+    const handleLikeClick = () => {
+        if (rating === 'like') {
+            setRating(undefined);
+        } else {
+            setRating('like');
+        }
+    };
+
+    const handleDislikeClick = () => {
+        if (rating === 'dislike') {
+            setRating(undefined);
+        } else {
+            setRating('dislike');
+        }
+    };
+
     return (
         <div className="rating">
-            <button  className="rating__likes"><Image src="/Like.svg" alt="like" width={13} height={11}/>Like {likes}</button>
-            <button  className="rating__dislikes"><Image src="/Dislike.svg" alt="like" width={13} height={11}/>Trash {dislikes}</button>
+            <button className={`rating__likes ${rating === 'like' ? 'liked' : ''}`} onClick={handleLikeClick} ><Image src="/Like.svg" alt="like" width={13} height={11} />Like {likes}</button>
+            <button className={`rating__dislikes ${rating === 'dislike' ? 'disliked' : ''}`} onClick={handleDislikeClick}><Image src="/Dislike.svg" alt="like" width={13} height={11} />Trash {dislikes}</button>
         </div>
     )
 }
