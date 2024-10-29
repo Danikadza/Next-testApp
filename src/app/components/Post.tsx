322553e14faf9b02312f333ef56564a6237ad1af
Danikadza <gurianov2000@yandex.ru>
2024-10-29 17:06:12 +0300

    feat/ пропсы и стили для компонента постов

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Rating from "./Rating";
 
-const Post = () => {
+interface PostProps {
+    id: string;
+    title: string;
+    text: string;
+    date: string;
+    likes: number;
+    dislikes: number;
+    tags: string;
+}
+
+const Post = ({title, text, date, likes, dislikes, tags, id}: PostProps) => {
     return (
         <div className="post">
-            <h1 className="post__header">Post1</h1>
-            <div className="post__text">Post text</div>
+            <h1 className="post__title">{title}</h1>
+            <div className="post__text">{text}</div>
             <div className="post__info">
                 <Rating/>
                 <button className="post__info__comments">Open comments</button>
-                <div className="post__info__date">date</div>
-                <div className="post__info__tags">Tag</div>
+                <div className="post__info__date">{date}</div>
+                <div className="post__info__tags">{tags}</div>
             </div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Post from "./components/Post";
 
-export default function Home() {
+async function fetchData() {
+  const data = await fetch('https://dummyjson.com/posts');
+  const result = await data.json();
+  return result.posts
+}
+ 
+
+export default async function Home() {
+
+  const posts = await fetchData();
+
+
+
   return (
     <div className="post__group">
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post id={posts[0].id} title={posts[0].title} text={posts[0].body} date="Today" tags={posts[0].tags} likes={posts[0].reactions.likes} dislikes={posts[0].reactions.dislikes}/>
     </div>
   );
 }
