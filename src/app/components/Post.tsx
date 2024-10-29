74a382d5423f5a7d6f5745782558136c143f9e0e
Danikadza <gurianov2000@yandex.ru>
2024-10-29 16:29:27 +0300

    feat/ стили для страницы постов

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -3,11 +3,14 @@ import Rating from "./Rating";
 
 const Post = () => {
     return (
-        <div>
-            <h1>Post1</h1>
-            <div>Post text</div>
-            <div>
+        <div className="post">
+            <h1 className="post__header">Post1</h1>
+            <div className="post__text">Post text</div>
+            <div className="post__info">
                 <Rating/>
+                <button className="post__info__comments">Open comments</button>
+                <div className="post__info__date">date</div>
+                <div className="post__info__tags">Tag</div>
             </div>
         </div>
     )
