8bc448028932437d29954973ac2224142ce7e66e
Danikadza <gurianov2000@yandex.ru>
2024-10-29 17:15:59 +0300

    feat/ пропсы для рейтинга и стили

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Rating from "./Rating";
+import Link from "next/link";
 
 interface PostProps {
     id: string;
@@ -17,8 +18,8 @@ const Post = ({title, text, date, likes, dislikes, tags, id}: PostProps) => {
             <h1 className="post__title">{title}</h1>
             <div className="post__text">{text}</div>
             <div className="post__info">
-                <Rating/>
-                <button className="post__info__comments">Open comments</button>
+                <Rating likes={likes} dislikes={dislikes}/>
+                <Link href="/" className="post__info__comments">Open comments</Link>
                 <div className="post__info__date">{date}</div>
                 <div className="post__info__tags">{tags}</div>
             </div>
--- src/app/components/Rating.tsx
+++ src/app/components/Rating.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Image from "next/image";
 
-const Rating = () => {
+interface RatingProps {
+    likes: number;
+    dislikes: number;
+}
+
+const Rating = ({likes, dislikes} : RatingProps) => {
     return (
         <div className="rating">
-            <button  className="rating__likes"><Image src="/Like.svg" alt="like" width={13} height={11}/>Like</button>
-            <button  className="rating__dislikes"><Image src="/Dislike.svg" alt="like" width={13} height={11}/>Trash</button>
+            <button  className="rating__likes"><Image src="/Like.svg" alt="like" width={13} height={11}/>Like {likes}</button>
+            <button  className="rating__dislikes"><Image src="/Dislike.svg" alt="like" width={13} height={11}/>Trash {dislikes}</button>
         </div>
     )
 }
