af288daff60b98a66a937f99759b62f18d7b223f
Danikadza <gurianov2000@yandex.ru>
2024-11-08 15:45:20 +0300

    feat/ изменение слова комментарий от количества

--- src/app/components/Comments.tsx
+++ src/app/components/Comments.tsx
@@ -29,13 +29,25 @@ interface CommentProps {
     }
 }
 
+function getCommentWord(count:number) {
+    if (count % 10 === 1 && count % 100 !== 11) {
+        return 'комментарий';
+    } else if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 12 || count % 100 > 14)) {
+        return 'комментария';
+    } else {
+        return 'комментариев';
+    }
+}
+
 const Comments = (commentsData: CommentsProps) => {
 
     const comments = commentsData.comments
 
+    const commentWord = getCommentWord(commentsData.total);
+
     return (
         <>
-        <h1>{commentsData.total} комментариев</h1>
+        <h1>{commentsData.total} {commentWord}</h1>
         {
         comments.map((comment:CommentProps) => 
           <Comment key={comment.id} postId={comment.postId} id={comment.id} user={comment.user} body={comment.body} likes={comment.likes} />
